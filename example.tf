dd5defd773746ab367ff3be91dd57a4801d7efa0
MeghanLiese <meghan@hashicorp.com>
2017-10-24 15:38:21 -0700

    update creds

--- example.tf
+++ example.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  2   access_key = "AKIAJJX63H2Z4R4MC2IA"
-  3   secret_key = "s7BRaIUlgAq+ofDJkiB4qMlZoHBTpMoXzGXLDj0w"
-  4   region     = "us-east-1"
-  5 }
-  6 
-  7 resource "aws_instance" "example" {
-  8   ami           = "ami-2757f631"
-  9   instance_type = "t2.micro"
- 10 }
+  access_key = "AKIAIXJBVJ7RHLPLMNGA"
+  secret_key = "S5WBRJPBrTwW2w89QqgKf7R2ZYiHcUaM3yv2AOzF"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
